8f96b86ca0466cee2be00f8f04a6217b6b3fd1b9
Béci Szabó <szabo.beci10@gmail.com>
2023-10-28 12:16:28 +0200

    ISzuperhos letrehozása

---
+++ SzuperhosProjekt/ISzuperhos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    internal interface ISzuperhos
+    {
+        public bool LegyoziE(ISzuperhos szuperhos);
+        public double MekkoraAzEreje();
+    }
+}
